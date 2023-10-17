ca292e041c4cbc9410ea4cba68bdd5ac66076859
Marco Bispo <marcobispo97@gmail.com>
2023-10-16 23:02:23 -0300

    Gráfico de barras

--- Dashboard.py
+++ Dashboard.py
@@ -22,19 +22,17 @@ dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(
     dados['Data da Compra'], format='%d/%m/%Y')
 
-
-# Tabelas
 receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
 recebita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(
     receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
-
 receita_mensal = dados.set_index('Data da Compra').groupby(
     pd.Grouper(freq='M'))['Preço'].sum().reset_index()
 receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
 receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
 
+receita_categorias = dados.groupby('Categoria do Produto')[
+    'Preço'].sum().sort_values(ascending=False)
 
-# Gráficos
 fig_mapa_receita = px.scatter_geo(recebita_estados,
                                   lat='lat',
                                   lon='lon',
@@ -43,26 +41,46 @@ fig_mapa_receita = px.scatter_geo(recebita_estados,
                                   template='seaborn',
                                   hover_name='Local da compra',
                                   hover_data={'lat': False, 'lon': False},
-                                  title='Recebita por estado'
+                                  title='Receita por estado'
                                   )
 
 fig_receita_mensal = px.line(receita_mensal,
                              x='Mes',
                              y='Preço',
                              markers=True,
-                             range_y=(0, receita_mensal.max()),
+                             range_y=(0, receita_mensal['Preço'].max()),
                              color='Ano',
                              line_dash='Ano',
                              title='Receita mensal'
                              )
 fig_receita_mensal.update_layout(yaxis_title='Receita')
 
+receita_estados = receita_estados.sort_values(
+    by='Preço', ascending=True).head(10)
+receita_estados.reset_index(inplace=True)
+st.dataframe(receita_estados)
+fig_receita_estados = px.bar(
+    receita_estados.head(10),
+    x=receita_estados.iloc[:, 0],
+    y=receita_estados.iloc[:, 1],
+    text_auto=True,
+    title='Top estados (receita)')
+
+fig_receita_categorias = px.bar(receita_categorias,
+                                text_auto=True,
+                                title='Receita por categoria')
+
+fig_receita_categorias.update_layout(yaxis_title='Receita')
+
 coluna1, coluna2 = st.columns(2)
 with coluna1:
     st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
     st.plotly_chart(fig_mapa_receita, use_container_width=True)
+    st.plotly_chart(fig_receita_estados, use_container_width=True)
+
 with coluna2:
     st.metric('Quantidade de vendas', formata_numero(dados.shape[0]))
     st.plotly_chart(fig_receita_mensal, use_container_width=True)
+    st.plotly_chart(fig_receita_categorias, use_container_width=True)
 
 st.dataframe(dados)
