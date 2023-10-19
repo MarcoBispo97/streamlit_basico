ccc2b7b120c79e7f1fe7c5c29ed51c36f0b27233
Marco Bispo <marcobispo97@gmail.com>
2023-10-18 21:50:53 -0300

    Download de arquivos

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -1,9 +1,23 @@
+import time
 import streamlit as st
 import requests
 import pandas as pd
 import plotly.express as px
 
+
+@st.cache_data  # optimiza a memória
+def converte_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso!', icon="✅")
+    time.sleep(5)
+    sucesso.empty()
+
+
 st.title('Dados brutos')
+
 
 url = 'https://labdados.com/produtos'
 
@@ -11,6 +25,10 @@ response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(
     dados['Data da Compra'], format='%d/%m/%Y')
+
+with st.expander('Colunas'):
+    colunas = st.multiselect('Selecione as colunas', list(
+        dados.columns), list(dados.columns))
 
 with st.sidebar.expander('Nome do produto'):
     produtos = st.multiselect(
@@ -41,5 +59,31 @@ with st.sidebar.expander('Quantidade de parcelas'):
     qtd_parcelas = st.slider(
         'Selecione a quantidade de parcelas', 1, 24, (1, 24))
 
+query = '''
+Produto in @produtos and \
+`Categoria do Produto` in @categoria and \
+@preco[0] <= Preço <= @preco[1] and \
+@frete[0] <= Frete <= @frete[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+Vendedor in @vendedores and \
+`Local da compra` in @local_compra and \
+@avaliacao[0]<= `Avaliação da compra` <= @avaliacao[1] and \
+`Tipo de pagamento` in @tipo_pagamento and \
+@qtd_parcelas[0] <= `Quantidade de parcelas` <= @qtd_parcelas[1]
+'''
 
-st.dataframe(dados)
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
+st.dataframe(dados_filtrados)
+st.markdown(
+    f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    nome_arquivo = st.text_input(
+        '', label_visibility='collapsed', value='dados')
+    nome_arquivo += '.csv'
+with coluna2:
+    st.download_button('Fazer o download da tabela em csv', data=converte_csv(
+        dados_filtrados), file_name=nome_arquivo, mime='text/csv', on_click=mensagem_sucesso)
